3f52545e303792195d468805177b267b398d699f
Adam Thompson <adam.thompson@uwaterloo.ca>
2016-12-18 17:08:22 -0500

    Initial Commit - First few sections designed

---
+++ resumeContentJSON.js
@@ -0,0 +1,229 @@
+resumeContent = {
+	experience: [
+	{
+		company: "Zynga",
+		term: "Spring 2016",
+		position: "Product Manager",
+		location: "Toronto, ON",
+		responsibilities: [
+		{
+			task: "Performed background research, user analysis, and A/B testing as part of the design process for a new Subscriptions model",
+			tags: ["research", "ux", "pm", "excel"]
+		},
+		{
+			task: "",
+			tags: []
+		},
+		{
+			task: "",
+			tags: []
+		},
+		{
+			task: "",
+			tags: []
+		}
+		]
+	},
+	{
+		company: "Noom",
+		term: "Fall 2015",
+		position: "Product Designer",
+		location: "New York, NY",
+		responsibilities: [
+		{	
+			task: "Leveraged user testing results to re-design the meal-logging experience, improving clarity, speed of navigation and to better reflect the common mental model of logging food",
+			tags: ["ux", "product design", "research", "ui", "sketch", "framer", "origami"]
+		},
+		{
+			task: "Initiated multidisciplinary collaboration with all stakeholders to better understand product requirements, and managed the early design process for the coach dashboard",
+			tags: ["pm", "ux"]
+		},
+		{
+			task: "Enhanced the food portion picker interface to improve user confidence when logging food items",
+			tags: ["ui", "product design", "ux", "sketch"]
+		},
+		{
+			task: "Prototyped and iterated interaction designs using Sketch, Framer JS, and Origami, and tested using Usability Hub and UserTesting.com",
+			tags: ["ux", "ui", "sketch", "framer", "js", "origami"]
+		}]
+	},
+	{
+		company: "KnowRoaming",
+		term: "Spring 2014 & Winter 2015",
+		position: ["Product Designer", "UX Designer", "Product Manager"],
+		location: "Toronto, ON",
+		responsibilities: [
+		{
+		task: "Redesigned both iOS and Android apps from the ground up. I reworked everything from the registration workflow, to the in app experience at home and abroad using the KnowRoaming SIM Sticker.",
+		tags: ["ux", "pm", "product", "design", "ucd"]
+		},
+		{
+			task: "Leveraged customer feedback to iterate on the flow of the app to better correspond with the users' mental model of the 3 use-phases of the product.",
+			tags: ["ux", "research", "ucd"]
+		},
+		{
+			task: "Initiated and oversaw the preliminary design of the new company homepage, and corporate portal",
+			tags: ["ux", "pm", "web"]
+		},
+		{
+			task: "Storyboarded, and animated an app intro video for the updated KnowRoaming app",
+			tags: ["ux", "ui", "animation", "video", "media"]
+		},
+		{
+			task: "Designed and developed the KnowRoaming Athletes sign-up page using HTML5, CSS3, CoffeeScript and PHP.",
+			tags: ["ux", "web", "development", "ui", "html", "css", "js", "php"]
+		},
+		{
+			task: "Started and lead weekly design meetings with developers, customer service, marketing and company leadership to discuss the experience of the mobile apps, consumer facing and corporate websites, and other relevant topics.",
+			tags: ["pm", "leader", "manager"]
+		},
+		{
+			task: "Created, branded and optimized a core company service, now called ReachMe.",
+			tags: ["pm", "marketing", "branding"]
+		},
+		{
+			task: "Designed and prototyped a retail stand for dispensing product boxes and displaying product information in retail stores.",
+			tags: ["id", "product", "design", "prototype", "retail"]
+		},
+		{
+			task: "Designed a new product package concept, reducing the box side by 50% by leveraging unique features of the KnowRoaming product.",
+			tags: ["id", "packaging", "retail", "optimization"]
+		},
+		{
+			task: "Created 3D concept models of an automated sticker application device using AutoCAD and Inventor",
+			tags: ["id", "modelling", "product", "design", "autocad", "inventor", "3d"]
+		}
+		]
+	},
+	{
+		company: "Sony",
+		term: "Fall 2013",
+		position: ["UX Designer", "User Experience Designer"],
+		location: "Waterloo, ON",
+		responsibilities: [
+		{
+			task: "Lead the design and development for a new music-dicovery/radio feature in MediaGo.",
+			tags: ["design", "development", "ps", "illustrator", "pm", "ui", "ux"]
+		},
+		{
+			task: "Developed the front-end for the new feature using HTML, SASS and jQuery",
+			tags: ["ui", "web", "development", "html", "css", "sass", "js"]
+		},
+		{
+			task: "Proposed a UI re-design of the entire MediaGo application, enhancing aesthetics, and ove user experience",
+			tags: ["ui", "product", "design", "ux", "media"]
+		},
+		{
+			task: "Designed promotional graphics and videos for MediaGo",
+			tags: ["ui", "graphic", "design", "animation", "video", "media"]
+		}
+		]
+	}],
+
+	projects: [
+	{
+		name: "Audity Hearing Protection",
+		term: "Sept. 2016 - present",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		responsibilities: [
+		{},
+		]
+	},
+
+	{
+		name: "SMRT WATR",
+		term: "Spring 2015",
+		description: "An interactive fountain with dynamic water spouts controlled by an Arduino, connected via a Raspberry Pi to enable users to play a quiz game with the fountain, which reacted to the quiz responses",
+		responsibilities: [
+		{
+			task: "Designed the in-game experience, app structure, and UI",
+			tags: []
+		},
+		{
+			task: "Designed the mechanism to allow the servo motor to rotate the fountain jet",
+			tags: []
+		},
+		{
+			task: "Developed the web-app front-end, and a portion of the Python server",
+			tags: []
+		},
+		{
+			task: "Designed and built the wood and plastic frame and water basin",
+			tags: []
+		}
+		]
+	},
+	{
+		name: "Colour Thermometer",
+		term: "Winter 2015",
+		description: "A web app that represents the current temperature, weather conditions and time as an HSV colour.",
+		responsibilities: []
+	},
+	{
+		name: "Analog Single-Note Guitar Tuner",
+		term: "August 2015",
+		description: "An analog circuit that uses a series of band-pass filters, and envelope detector, and rectifier to discern whether the high-E string is in tune.",
+		responsibilities: []
+	},
+	{
+		name: "Say No To Comic Sans",
+		term: "Spring 2013",
+		description: "A Chrome extension that scans a webpage for Comic Sans, and replaces it with Helvetica.",
+		responsibilities: []
+	},
+	{
+		name: "Arduino VU Meter",
+		term: "August 2013",
+		description: "An Arduino system that takes samples from a mono-audio channel and outputs the relative volume on an array of LEDs",
+		responsibilities: []
+	},
+	{
+		name: "STiC - Ski Tip Connector",
+		term: "Fall 2012",
+		description: "A mechanical device to allow instructors of disabled skiers to more easily control and guide their students.",
+		responsibilities: [
+		{
+			task: "Designed the rod-and-ring mechanism to ensure the wearer’s skis do not cross",
+			tags: []
+		},
+		{
+			task: "Designed and modelled the device body and built the initial prototype",
+			tags: []
+		}
+		]
+	},
+	],
+
+	education: [
+	{
+		school: "University of Waterloo",
+		program: "Systems Design Engineering",
+		degree: "BASc",
+		minor: "Cognitive Science",
+		courses: [
+		{
+			name: "",
+			tags: []
+		},
+		]
+	}
+	],
+
+	skills: ["Sketch", "Photoshop", "Illustrator", "Balsamiq", "HTML 5", "CSS 3", "jQuery", "CoffeeScript", "AutoCad", "Inventor", "3DS Max", "Cinema 4D", "Arduino", "After Effects", "Final Cut X", "InDesign", "Swift", "PHP", "C++", "Java", "Git", "XCode", "Bootstrap", "SASS"],
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
