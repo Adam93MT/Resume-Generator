8a100f54ef527a0fe631a338e9e77891426c5bab
adam <adam@adamthompson.ca>
2017-06-21 14:23:02 -0400

    Added Cover Letter Markdown Parsing

--- generateResume.py
+++ generateResume.py
@@ -264,4 +264,20 @@ if ("cl" in sys.argv):
 	print "Compiling Cover Letter"
 	clName = "cover_letters/Adam Thompson - " + jobTitle.title() + ".pdf"
 	clName = clName.replace(" ", "\ ")
+
+	mdtext = ""
+	with open('cover_letters/' + jobTitle + '.md', 'r') as mdfile:
+		for line in mdfile:
+			newline = line.strip()
+			if newline:
+				newline = newline.replace("\'", "\\'")
+				mdtext += newline + "\\n\\n"
+
+	# Since Prince doesn't do AJAX, we need to save the .md file as a js variable which is loaded at runtime
+	with open('cover_letters/tmpcl.js', 'w') as tmpmdfile:
+		tmpmdfile.write("clContent = '" + mdtext + "'")
+
 	os.system("prince --javascript -s resume-style.css coverLetter.html " + clName)
+
+
+
--- resources/js/compileCoverLetter.js
+++ resources/js/compileCoverLetter.js
@@ -1,1 +1,8 @@
-var showdown = require('showdown')
+converter = new showdown.Converter()
+text = clContent + "\n\n Sincerely,"
+html = converter.makeHtml(text);
+
+$(document).ready(function(){
+	$('.cover_letter_content').html(html)
+});
+
